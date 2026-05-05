c63530604fe97175238925d915e11f4ac336405a
cusine <kwoojin05@gmail.com>
2026-05-05 16:57:39 +0900

    추가

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 // E키로 인벤토리를 열고 닫으며, 열려 있을 때 핫바를 숨기는 컴포넌트
 public class InventoryManager : MonoBehaviour
 {
     public GameObject inventoryUI;
     public GameObject hotbarUI;
-
     private bool isInventoryOpen = false;
-
+    private float previousTimeScale = 1f;
+    public GameObject firstSlot; // 인벤토리의 첫 번째 슬롯 버튼
     void Start()
     {
         // 게임 시작 시 인벤토리 닫힘 상태로 초기화
@@ -15,19 +15,29 @@ public class InventoryManager : MonoBehaviour
         inventoryUI.SetActive(false);
         hotbarUI.SetActive(true);
     }
-
     void Update()
     {
         // E키로 인벤토리 열기/닫기 전환
         if (Input.GetKeyDown(KeyCode.E))
             ToggleInventory();
     }
-
     // 인벤토리와 핫바 표시 상태를 반전
     public void ToggleInventory()
     {
         isInventoryOpen = !isInventoryOpen;
         inventoryUI.SetActive(isInventoryOpen);
         hotbarUI.SetActive(!isInventoryOpen);
+        if (isInventoryOpen)
+        {
+            previousTimeScale = Time.timeScale;
+            Time.timeScale = 0f;
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(firstSlot);
+        }
+        else
+        {
+            Time.timeScale = previousTimeScale;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
     }
 }
